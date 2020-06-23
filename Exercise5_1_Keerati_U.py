88c6b8f6b0d3404d4b1f3aa66fcafd2b0a8d2ce8
btxonline <59719135+btxonline@users.noreply.github.com>
2020-06-23 23:09:40 +0700

    Create Exercise5_1_Keerati_U.py

---
+++ Exercise5_1_Keerati_U.py
@@ -0,0 +1,7 @@
+x = float(input('First Number:'))
+y = float(input('Second Number:'))
+
+print(f'{x} + {y} = {x+y}')
+print(f'{x} - {y} = {x-y}')
+print(f'{x} * {y} = {x*y}')
+print(f'{x} / {y} = {x/y}')
